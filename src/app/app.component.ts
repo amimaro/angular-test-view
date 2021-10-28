0bb4bd0879e3370147050c1fa2db1e8f2cc8ebb0
amimaro <amir.zahlan@gmail.com>
2021-10-28 17:11:53 -0300

    Test views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-test-view';
+  @ViewChild('btn') btn: ElementRef | undefined;
+  @ViewChildren('btn') btnChildrens: QueryList<ElementRef> | undefined;
+
+  ngAfterViewInit(): void {
+    console.log('btn', this.btn);
+    console.log('btnChildrens', this.btnChildrens);
+
+    this.btnChildrens?.forEach((child) => {
+      console.log('child', child);
+      console.log('childInnerText', child.nativeElement.innerText);
+    });
+  }
 }
